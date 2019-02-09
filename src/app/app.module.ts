8ae38eb68bab6d806f0d09a871447711e0aad935
Courtney Revada <revadacourtney@gmail.com>
2019-02-09 14:37:16 -0500

    New Components
    Created new components for app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatButtonModule, MatCheckboxModule} from '@angular/material';
 
 import { AppComponent } from './app.component';
+import { ProfileNameComponent } from './profile-name/profile-name.component';
+import { SummaryComponent } from './summary/summary.component';
+import { ProgressBarComponent } from './progress-bar/progress-bar.component';
+import { ExperienceComponent } from './experience/experience.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProfileNameComponent,
+    SummaryComponent,
+    ProgressBarComponent,
+    ExperienceComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatCheckboxModule
   ],
   providers: [],
   bootstrap: [AppComponent]
