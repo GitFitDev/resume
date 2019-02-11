875c100bfaff99c5c0b89bfa58cea17e2d8d4cc6
Courtney Revada <revadacourtney@gmail.com>
2019-02-10 22:38:50 -0500

    Touch Ups
    Lots of Boss changes

--- src/app/profile-name/profile-name.component.ts
+++ src/app/profile-name/profile-name.component.ts
@@ -6,8 +6,41 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./profile-name.component.css']
 })
 export class ProfileNameComponent implements OnInit {
-
-  constructor() { }
+  technicalSkills = [];
+  professionalExperiences = [];
+  constructor() { 
+    this.technicalSkills.push(
+      {technicalSkill: 'JavaScript', experienceValue: '65'},
+      {technicalSkill: 'HTML', experienceValue: '75'},
+      {technicalSkill: 'CSS', experienceValue: '60'},
+      {technicalSkill: 'Angular', experienceValue: '50'},
+    );
+    this.professionalExperiences.push(
+      {
+        professionalExperience: 'MindRight', 
+        jobTitle: 'Software Engineer Intern',
+        startDate: 'November 2018',
+        endDate: 'Present',
+        responsibilities: [
+         'Responsible for maintaining and developing on semi-conductor monitoring software (Cross platform desktop app built with Electron',
+         'Responsible for maintaining and developing on semi-conductor monitoring software (Cross platform desktop app built with Electron',
+         'Responsible for maintaining and developing on semi-conductor monitoring software (Cross platform desktop app built with Electron'
+        ]
+      },
+      {
+        professionalExperience: 'Starbucks', 
+        jobTitle: 'Software Engineer Intern',
+        startDate: 'November 2018',
+        endDate: 'Present',
+        responsibilities: [
+         'Responsible for maintaining and developing on semi-conductor monitoring software (Cross platform desktop app built with Electron',
+         'Responsible for maintaining and developing on semi-conductor monitoring software (Cross platform desktop app built with Electron',
+         'Responsible for maintaining and developing on semi-conductor monitoring software (Cross platform desktop app built with Electron'
+        ]
+      },
+      
+    );
+  }
 
   ngOnInit() {
   }
